22b9ec6c56ca5e1241463e816e5b7b58cbd9d3ba
kirides <13602143+kirides@users.noreply.github.com>
2025-06-21 07:51:16 +0200

    update prepare redist

--- PrepareRedist.bat
+++ PrepareRedist.bat
@@ -7,12 +7,10 @@ CD "%~dp0"
 mkdir "%TARGET_DIR%\GD3D11\shaders"
 Xcopy "D3D11Engine\Shaders\*" "%TARGET_DIR%\GD3D11\shaders" /y /s
 
-mkdir "%TARGET_DIR%\GD3D11\shaders\CSFFT"
-COPY /y "D3D11Engine\CSFFT\*.hlsl" "%TARGET_DIR%\GD3D11\shaders\CSFFT"
-
 Xcopy "blobs\data" "%TARGET_DIR%\GD3D11\data\" /y /s
 Xcopy "blobs\Meshes" "%TARGET_DIR%\GD3D11\Meshes\" /y /s
 Xcopy "blobs\Textures" "%TARGET_DIR%\GD3D11\Textures\" /y /s
+Xcopy "blobs\Fonts" "%TARGET_DIR%\GD3D11\Fonts\" /y /s
 
 
 Xcopy "blobs\libs\*" "%TARGET_DIR%\" /y /s
