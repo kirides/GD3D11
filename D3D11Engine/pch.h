1555e9a7d6b3cba43a8317e7c4fdb220e86ae0d5
kirides <13602143+kirides@users.noreply.github.com>
2026-01-09 21:33:14 +0100

    enable SSE3 intrinsics for a debug build

--- D3D11Engine/pch.h
+++ D3D11Engine/pch.h
@@ -5,6 +5,10 @@
 
 #if !PUBLIC_RELEASE
 #define DEBUG_D3D11
+// assume SSE3 is available when compiling in debug mode for SSE2
+#if _M_IX86_FP == 2 // SSE2
+#define _XM_SSE3_INTRINSICS_
+#endif
 #endif
 
 #include <Windows.h>
