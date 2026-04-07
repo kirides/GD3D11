fac4ae28aaf172b23ab25a105c991d4ea085d4d9
kirides <13602143+kirides@users.noreply.github.com>
2026-04-07 10:09:35 +0200

    improve StringID debug-ability when #DEBUG_D3D11

--- D3D11Engine/StringID.h
+++ D3D11Engine/StringID.h
@@ -3,6 +3,29 @@
 
 class StringID {
 public:
+
+#ifdef DEBUG_D3D11
+    // This allows: StringID myId = "u_Texture"; 
+    // Computed at compile-time!
+    consteval StringID( std::string_view str ) 
+        : m_hash( SID( str ) ),
+        m_strView( str )
+    { }
+
+    template <size_t N>
+    consteval StringID( const char( &str )[N] ) 
+        : m_hash( SID( std::string_view( str, N - 1 ) ) ),
+        m_strView(str)
+    {}
+
+    static StringID make( std::string_view str ) {
+        StringID id = {};
+        id.m_hash = SID( str );
+        id.m_str = str;
+        id.m_strView = id.m_str;
+        return id;
+    }
+#else
     // This allows: StringID myId = "u_Texture"; 
     // Computed at compile-time!
     consteval StringID(std::string_view str) : m_hash(SID(str)) {
@@ -11,16 +34,21 @@ public:
     template <size_t N>
     consteval StringID( const char( &str )[N] ) : m_hash( SID( std::string_view( str, N - 1 ) ) ) {}
 
-    constexpr operator uint32_t() const { return m_hash; }
-
-    static StringID make(std::string_view str) {
+    static StringID make( std::string_view str ) {
         StringID id;
         id.m_hash = SID( str );
         return id;
     }
+#endif
+
+    constexpr operator uint32_t() const { return m_hash; }
 private:    
     StringID() = default;
     uint32_t m_hash;
+#ifdef DEBUG_D3D11
+    std::string_view m_strView;
+    std::string m_str;
+#endif
     
     static constexpr uint32_t SID(std::string_view str) {
         static_assert(sizeof(uint32_t) == sizeof(size_t), "StringID requires size_t to be 32 bits");
