d118ff3f0c4a53550e1ae756bd96a3990f16d6e2
kirides <13602143+kirides@users.noreply.github.com>
2026-04-23 14:12:33 +0200

    block zCSkyControler::ClearBackground as it causes issues when viewport != window size (upscaling)

--- D3D11Engine/GothicMemoryLocations1_12f.h
+++ D3D11Engine/GothicMemoryLocations1_12f.h
@@ -148,6 +148,7 @@ struct GothicMemoryLocations {
         static const unsigned int VTBL_RenderSkyPre = 20;
         static const unsigned int VTBL_RenderSkyPost = 21;
         static const unsigned int static_skyEffectsEnabled = 0x887EDC; // int*
+        static const unsigned int ClearBackground = 0;
     };
 
     struct zCParticleEmitter {
