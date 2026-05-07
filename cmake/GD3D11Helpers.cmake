dcb3acdda03684dd3a5389cca3be924e4875b145
kirides <13602143+kirides@users.noreply.github.com>
2026-05-07 10:11:03 +0200

    add clang support to cmake

--- cmake/GD3D11Helpers.cmake
+++ cmake/GD3D11Helpers.cmake
@@ -29,6 +29,123 @@ function(gd3d11_link_if_target target_name dependency_name)
   endif()
 endfunction()
 
+function(gd3d11_detect_compiler_family out_var)
+  if(CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
+    set(_family "CLANG")
+  elseif(MSVC)
+    set(_family "MSVC")
+  else()
+    set(_family "OTHER")
+  endif()
+
+  set(${out_var} "${_family}" PARENT_SCOPE)
+endfunction()
+
+function(gd3d11_validate_compiler_selection)
+  gd3d11_detect_compiler_family(_detected_family)
+
+  if(NOT DEFINED GD3D11_COMPILER OR GD3D11_COMPILER STREQUAL "")
+    set(_requested "AUTO")
+  else()
+    set(_requested "${GD3D11_COMPILER}")
+  endif()
+
+  if(_requested STREQUAL "AUTO")
+    message(STATUS "GD3D11 compiler mode: AUTO (detected ${_detected_family}, CXX=${CMAKE_CXX_COMPILER_ID})")
+    return()
+  endif()
+
+  if(_requested STREQUAL "MSVC" AND NOT _detected_family STREQUAL "MSVC")
+    message(FATAL_ERROR "GD3D11_COMPILER=MSVC was requested, but detected CXX compiler is '${CMAKE_CXX_COMPILER_ID}'. Use an MSVC preset/toolchain or set GD3D11_COMPILER=AUTO.")
+  endif()
+
+  if(_requested STREQUAL "CLANG" AND NOT _detected_family STREQUAL "CLANG")
+    message(FATAL_ERROR "GD3D11_COMPILER=CLANG was requested, but detected CXX compiler is '${CMAKE_CXX_COMPILER_ID}'. Use a clang preset/toolchain or set GD3D11_COMPILER=AUTO.")
+  endif()
+
+  message(STATUS "GD3D11 compiler mode: ${_requested} (CXX=${CMAKE_CXX_COMPILER_ID})")
+endfunction()
+
+function(gd3d11_apply_windows_sdk_includes target_name)
+  gd3d11_detect_compiler_family(_compiler_family)
+
+  if(NOT WIN32 OR _compiler_family STREQUAL "MSVC")
+    return()
+  endif()
+
+  set(_candidate_include_dirs)
+
+  set(_env_include "$ENV{INCLUDE}")
+  if(NOT _env_include STREQUAL "")
+    string(REPLACE "\\" "/" _env_include "${_env_include}")
+    foreach(_include_dir IN LISTS _env_include)
+      if(EXISTS "${_include_dir}")
+        list(APPEND _candidate_include_dirs "${_include_dir}")
+      endif()
+    endforeach()
+  endif()
+
+  set(_sdk_dir "$ENV{WindowsSdkDir}")
+  if(_sdk_dir STREQUAL "")
+    set(_sdk_dir "$ENV{WindowsSdkDir_10}")
+  endif()
+  string(REPLACE "\\" "/" _sdk_dir "${_sdk_dir}")
+
+  set(_sdk_version "$ENV{WindowsSDKVersion}")
+  string(REPLACE "\\" "/" _sdk_version "${_sdk_version}")
+  string(REGEX REPLACE "/+$" "" _sdk_version "${_sdk_version}")
+
+  if(_sdk_dir STREQUAL "" AND EXISTS "C:/Program Files (x86)/Windows Kits/10")
+    set(_sdk_dir "C:/Program Files (x86)/Windows Kits/10")
+  endif()
+
+  if(NOT _sdk_dir STREQUAL "")
+    set(_sdk_include_root "${_sdk_dir}/Include")
+
+    if(_sdk_version STREQUAL "" AND EXISTS "${_sdk_include_root}")
+      file(GLOB _sdk_version_dirs LIST_DIRECTORIES true "${_sdk_include_root}/*")
+      list(SORT _sdk_version_dirs COMPARE NATURAL ORDER DESCENDING)
+      foreach(_version_dir IN LISTS _sdk_version_dirs)
+        if(EXISTS "${_version_dir}/um/windows.h" OR EXISTS "${_version_dir}/um/Windows.h")
+          get_filename_component(_sdk_version "${_version_dir}" NAME)
+          break()
+        endif()
+      endforeach()
+    endif()
+
+    if(NOT _sdk_version STREQUAL "")
+      foreach(_component IN ITEMS shared um ucrt winrt)
+        set(_component_dir "${_sdk_include_root}/${_sdk_version}/${_component}")
+        if(EXISTS "${_component_dir}")
+          list(APPEND _candidate_include_dirs "${_component_dir}")
+        endif()
+      endforeach()
+    endif()
+  endif()
+
+  list(REMOVE_DUPLICATES _candidate_include_dirs)
+
+  if(_candidate_include_dirs)
+    target_include_directories(${target_name} SYSTEM PRIVATE ${_candidate_include_dirs})
+  endif()
+
+  set(_has_windows_h FALSE)
+  set(_has_d3d11_h FALSE)
+
+  foreach(_include_dir IN LISTS _candidate_include_dirs)
+    if(EXISTS "${_include_dir}/windows.h" OR EXISTS "${_include_dir}/Windows.h")
+      set(_has_windows_h TRUE)
+    endif()
+    if(EXISTS "${_include_dir}/d3d11.h")
+      set(_has_d3d11_h TRUE)
+    endif()
+  endforeach()
+
+  if(NOT _has_windows_h OR NOT _has_d3d11_h)
+    message(WARNING "Best-effort Windows SDK include detection for non-MSVC target '${target_name}' could not verify both windows.h and d3d11.h. If compile fails, run from a Visual Studio Developer Prompt or set INCLUDE/WindowsSdkDir/WindowsSDKVersion.")
+  endif()
+endfunction()
+
 function(gd3d11_apply_common_compile_settings target_name)
   target_compile_definitions(${target_name} PRIVATE
     _DISABLE_CONSTEXPR_MUTEX_CONSTRUCTOR
@@ -115,6 +232,12 @@ function(gd3d11_apply_common_compile_settings target_name)
       target_link_options(${target_name} PRIVATE /LTCG /OPT:REF /OPT:ICF /DEBUG:FULL)
     endif()
   elseif(CMAKE_CXX_COMPILER_ID MATCHES "Clang|GNU")
+    if(WIN32)
+      target_compile_definitions(${target_name} PRIVATE _ALLOW_COMPILER_AND_STL_VERSION_MISMATCH)
+      target_compile_options(${target_name} PRIVATE -m32)
+      target_link_options(${target_name} PRIVATE -m32)
+    endif()
+
     target_compile_options(${target_name} PRIVATE -ffast-math)
 
     if(GD3D11_SIMD STREQUAL "AVX")
