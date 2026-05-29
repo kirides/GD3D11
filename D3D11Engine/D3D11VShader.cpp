cba31c385af48a75cfad651a9ca3b0c8a77e1e70
kirides <13602143+kirides@users.noreply.github.com>
2026-05-29 13:19:18 +0200

    remove duplicated layout

--- D3D11Engine/D3D11VShader.cpp
+++ D3D11Engine/D3D11VShader.cpp
@@ -76,13 +76,6 @@ namespace {
         { "TEXCOORD", 0, DXGI_FORMAT_R32G32_FLOAT, 0, D3D11_APPEND_ALIGNED_ELEMENT, D3D11_INPUT_PER_VERTEX_DATA, 0 },
     };
 
-    static const D3D11_INPUT_ELEMENT_DESC layout8[] =
-    {
-        { "POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, D3D11_APPEND_ALIGNED_ELEMENT, D3D11_INPUT_PER_VERTEX_DATA, 0 },
-        { "NORMAL", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, D3D11_APPEND_ALIGNED_ELEMENT, D3D11_INPUT_PER_VERTEX_DATA, 0 },
-        { "TEXCOORD", 0, DXGI_FORMAT_R32G32_FLOAT, 0, D3D11_APPEND_ALIGNED_ELEMENT, D3D11_INPUT_PER_VERTEX_DATA, 0 },
-    };
-
     static const D3D11_INPUT_ELEMENT_DESC layout9[] =
     {
         { "POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, D3D11_APPEND_ALIGNED_ELEMENT, D3D11_INPUT_PER_VERTEX_DATA, 0 },
@@ -171,7 +164,7 @@ namespace {
         {layout5, std::size( layout5 )},
         {layout6, std::size( layout6 )},
         {layout7, std::size( layout7 )},
-        {layout8, std::size( layout8 )},
+        {layout2, std::size( layout2 )}, // 8 and 2 were identical
         {layout9, std::size( layout9 )},
         {layout10, std::size( layout10 )},
         {layout11, std::size( layout11 )},
