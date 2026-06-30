8c72d254f45d0d91ce8ca3afdbb308b4c08289eb
kirides <13602143+kirides@users.noreply.github.com>
2026-06-30 21:42:58 +0200

    make hooks more ergonomical
    reduce UB

--- D3D11Engine/zSndMss.cpp
+++ D3D11Engine/zSndMss.cpp
@@ -12,17 +12,12 @@ void zCActiveSnd::AutoCalcObstruction( bool immediate ) {
     }
 
 #ifdef BUILD_GOTHIC_2_6_fix
-    using OriginalFuncType = void( __thiscall* )(void*, int);
-    OriginalFuncType original = reinterpret_cast<OriginalFuncType>(
-        HookedFunctions::OriginalFunctions.original_zCActiveSndAutoCalcObstruction
-    );
-
     if ( auto game = oCGame::GetGame() ) {
         if ( auto cam = (zCCamera*)game->_zCSession_camera ) {
             if ( zCCamera::GetCamera() != cam ) {
                 cam->Activate(); // set as active cam, otherwise obstruction is calculated wrong.
             }
-            original( this, immediate );
+            HookedFunctions::OriginalFunctions.original_zCActiveSndAutoCalcObstruction( this, immediate );
         }
     }
 #endif
