26681918af161f0fb589adb6565ef5cc25a27b88
kirides <13602143+kirides@users.noreply.github.com>
2026-07-23 19:45:05 +0200

    turn down default roughness

--- D3D11Engine/D3D12Engine/D3D12GraphicsEngine.cpp
+++ D3D11Engine/D3D12Engine/D3D12GraphicsEngine.cpp
@@ -1027,7 +1027,7 @@ bool D3D12GraphicsEngine::CreateWhiteTexture() {
 	}
 
 	CreateTexture( m_DefaultOrmTexture );
-	const uint32_t orm = 0xFF00E6FFu;
+	const uint32_t orm = 0xFF00D9FFu;
 	if ( XR_SUCCESS != m_DefaultOrmTexture->Init( INT2( 1, 1 ), GfxTexture::ETextureFormat::TF_R8G8B8A8, 1, &orm, "DefaultOrmTexture(1,0.9,0)" ) ) {
 		return false;
 	}
