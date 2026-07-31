906b1ed1e15f7391e4f29d6595daf1345a15b2b1
kirides <13602143+kirides@users.noreply.github.com>
2026-07-31 09:50:24 +0200

    back to 32 bit floats for now

--- D3D11Engine/Shaders/D3D12/TAAResolve.hlsl
+++ D3D11Engine/Shaders/D3D12/TAAResolve.hlsl
@@ -58,7 +58,7 @@
 
 // Whether to use real 16-bit floats. Requires SM6.2 and "-enable-16bit-types"; see note 5 above.
 #ifndef USE_FP16
-#define USE_FP16 1
+#define USE_FP16 0
 #endif
 
 typedef float  fp32_t;
